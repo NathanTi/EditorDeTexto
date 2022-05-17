7e5a04fa2695855c607c35374b6451068608e4f9
Teste <nathanbertonti@hotmail.com>
2022-05-16 21:19:04 -0300

    codigo convertido para o ingles

--- main.py
+++ main.py
@@ -3,89 +3,98 @@ from tkinter.filedialog import *
 from tkinter.messagebox import *
 
 
-class ProgramaPrincipal:
-    def __init__(self, telaVertical, telaHorizontal):
-        self.tamanhoTelaVertical = telaVertical
-        self.tamanhoTelaHorizontal = telaHorizontal
+# Classe para criação dos principais elementos gráficos.
+class MainDesign:
+    def __init__(self, screenHeight, screenWidth):
+        # Tamanho de tela em que o programa será exibido.
+        self.screenHeight = screenHeight
+        self.screenWidth = screenWidth
 
-    def criarTela(self):
+    def createAppScreen(self):
         # Função que define as caracteristicas da tela principal.
         root.title("Editor De Texto Python")
-        root.minsize(width=self.tamanhoTelaHorizontal, height=self.tamanhoTelaVertical)
-        root.maxsize(width=self.tamanhoTelaHorizontal, height=self.tamanhoTelaVertical)
+        root.minsize(width=self.screenWidth, height=self.screenHeight)
+        root.maxsize(width=self.screenWidth, height=self.screenHeight)
         root.tk_setPalette(background='dimgrey', activeBackground='white', activeForeground='black',
                            foreground='snow', insertBackground='black', selectBackground='black', )
 
-    def criarNavbar(self):
+    def createAppNavbar(self):
+        # Função que criar a barra de navegação superior do programa(novo arquivo e etc).
         navbar = Menu(root)
         menuNavbar = Menu(navbar, tearoff=0)
-        menuNavbar.add_command(label="Novo Arquivo", command=AdmArquivos().criarArquivo, font=("Arial", 10))
-        menuNavbar.add_command(label="Abrir Arquivo", command=AdmArquivos().abrirArquivo, font=("Arial", 10))
-        menuNavbar.add_command(label="Salvar", command=AdmArquivos().salvarArquivo, font=("Arial", 10))
-        menuNavbar.add_command(label="Sair", command=AdmErros("Fechar Programa ?", "Seu Progresso Não Será Salvo !").msgFecharPrograma,
+        menuNavbar.add_command(label="Novo Arquivo", command=FileAdm().createFile, font=("Arial", 10))
+        menuNavbar.add_command(label="Abrir Arquivo", command=FileAdm().openFile, font=("Arial", 10))
+        menuNavbar.add_command(label="Salvar", command=FileAdm().saveFile, font=("Arial", 10))
+        menuNavbar.add_command(label="Sair", command=ErrorAdm("Fechar Programa ?", "Seu Progresso Não Será Salvo !").msgShowCloseProgram,
                                font=("Arial", 10))
         navbar.add_cascade(label="Arquivos", menu=menuNavbar)
         root.config(menu=navbar)
 
 
-class AdmArquivos:
+# Classe para gerenciamento de arquivos.
+class FileAdm:
     def __init__(self):
-        self.nomeArquivo = "Novo Arquivo"
+        pass
 
-    def criarArquivo(self):
+    def createFile(self):
         # Função que cria um novo arquivo.
-        try:
-            textoPrincipal.delete(0.0, END)
-        except:
-            AdmErros("Erro ao Criar Arquivo", "Impossível Criar Novo Arquivo !!!").msgMostrarErro()
+        msgCreate = askquestion(title="Criar Novo Arquivo", message="Deseja Criar Novo Arquivo ?")
+        if msgCreate == 'yes':
+            try:
+                mainText.delete(0.0, END)
+            except:
+                ErrorAdm("Erro ao Criar Arquivo", "Impossível Criar Novo Arquivo !!!").msgShowError()
 
-    def salvarArquivo(self):
+    def saveFile(self):
         # Função que salva um arquivo.
-        arquivo = asksaveasfile(mode='w', defaultextension='.txt')
-        texto = textoPrincipal.get(0.0, END)
+        file = asksaveasfile(mode='w', defaultextension='.txt')
+        text = mainText.get(0.0, END)
         try:
-            arquivo.write(texto)
+            file.write(text)
         except:
-            AdmErros("Erro ao Salvar", "Impossível Salvar o Arquivo !!!").msgMostrarErro()
+            ErrorAdm("Erro ao Salvar", "Impossível Salvar o Arquivo !!!").msgShowError()
 
-    def abrirArquivo(self):
+    def openFile(self):
         # Função que abre um arquivo para edição.
         try:
-            arquivo = askopenfile(mode='r')
-            texto = arquivo.read()
-            textoPrincipal.delete(0.0, END)
-            textoPrincipal.insert(0.0, texto)
+            file = askopenfile(mode='r')
+            text = file.read()
+            mainText.delete(0.0, END)
+            mainText.insert(0.0, text)
         except:
-            AdmErros("Erro ao Abrir Arquivo", "Impossível Abrir o Arquivo !!!").msgMostrarErro()
+            ErrorAdm("Erro ao Abrir Arquivo", "Impossível Abrir o Arquivo !!!").msgShowError()
 
 
-class AdmErros:
-    def __init__(self, titulo, texto):
-        self.titulo = titulo
-        self.texto = texto
+# Classe para gerenciamento de erros.
+class ErrorAdm:
+    def __init__(self, title, text):
+        # Titulo e Conteúdo da mensagem de erro.
+        self.title = title
+        self.text = text
 
-    def msgMostrarErro(self):
+    def msgShowError(self):
         # Função que mostra um erro ao usuário utilizando o messagebox.
-        showerror(title=self.titulo, message=self.texto)
+        showerror(title=self.title, message=self.text)
 
-    def msgFecharPrograma(self):
-        # Funcionando
-        sair = askquestion(title=self.titulo, message=self.texto)
-        if (sair == 'yes'):
+    def msgShowCloseProgram(self):
+        # Função que pergunta se o usuário deseja fechar o programa.
+        msgClose = askquestion(title=self.title, message=self.text)
+        if msgClose == 'yes':
             root.quit()
 
 
-# Criação da Tela Principal.
-root = Tk()
-programa = ProgramaPrincipal(500, 500)
-programa.criarTela()
+if __name__ == "__main__":
+    # Criação da Tela Principal.
+    root = Tk()
+    textEditor = MainDesign(500, 500)
+    textEditor.createAppScreen()
 
-# Criação da caixa de texto.
-textoPrincipal = Text(root, width=programa.tamanhoTelaHorizontal, height=programa.tamanhoTelaVertical, font=("Arial", 20))
-textoPrincipal.pack()
+    # Criação da caixa de texto.
+    mainText = Text(root, width=500, height=500, font=("Arial", 20))
+    mainText.pack()
 
-# Criação da barra de navegação.
-programa.criarNavbar()
+    # Criação da barra de navegação.
+    textEditor.createAppNavbar()
 
-# Exibe a tela.
-root.mainloop()
+    # Exibe a tela.
+    root.mainloop()
